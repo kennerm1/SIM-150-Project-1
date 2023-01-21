cae94bf27fe68df253b7295378147106269fc439
kennerm1 <123021312+kennerm1@users.noreply.github.com>
2023-01-21 13:12:12 -0700

    Start SIM 150 Test

--- SIM 150 Test Project/Assets/Player.cs
+++ SIM 150 Test Project/Assets/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,14 +13,21 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if   (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.forward * 0.5f;
         }
-
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.back * 0.5f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.5f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.5f;
         }
     }
 }
